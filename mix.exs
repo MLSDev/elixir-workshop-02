7570f45b940839dc6d63d7bf43251b7c389b5b4a
Anton Sakharov <sakharov@mlsdev.com>
2018-07-19 01:20:33 +0200

    ExCoveralls added

--- mix.exs
+++ mix.exs
@@ -10,7 +10,9 @@ defmodule HolidayApp.Mixfile do
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.html": :test]
     ]
   end
 
@@ -42,7 +44,8 @@ defmodule HolidayApp.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:ex_machina, "~> 2.2", only: :test},
-      {:timex, "~> 3.0"}
+      {:timex, "~> 3.0"},
+      {:excoveralls, "~> 0.8", only: :test}
     ]
   end
 
